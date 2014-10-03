23ef8247339dce03ca42580730d8c2cffc191f03
Ruben_Cordeiro <ruben.cordeiro.eng@gmail.com>
2014-10-03 11:28:04 +0100

    Add coverage tests to the client
    Add the karma-coverage plugin to the karma test runner configuration file. The coverage analysis report is saved in the "coverage" folder. More information: https://github.com/karma-runner/karma-coverage.

--- Client/karma.conf.js
+++ Client/karma.conf.js
@@ -1,33 +1,50 @@
-module.exports = function(config){
-  config.set({
+module.exports = function (config) {
+    config.set({
 
-    basePath : './',
+        basePath: './',
 
-    files : [
-      'app/bower_components/angular/angular.js',
-      'app/bower_components/angular-ui-router/release/angular-ui-router.js',
-      'app/bower_components/angular-mocks/angular-mocks.js',
-      'app/components/**/*.js',
-      'app/view*/**/*.js'
-    ],
+        files: [
+            'app/bower_components/angular/angular.js',
+            'app/bower_components/angular-ui-router/release/angular-ui-router.js',
+            'app/bower_components/angular-mocks/angular-mocks.js',
+            'app/components/**/*.js',
+            'app/view*/**/*.js'
+        ],
 
-    autoWatch : true,
+        autoWatch: true,
 
-    frameworks: ['jasmine'],
+        // coverage reporter generates the coverage
+        reporters: ['progress', 'coverage'],
 
-    browsers : ['Chrome'],
+        preprocessors: {
+            // source files, that you wanna generate coverage for
+            // do not include tests or libraries
+            // (these files will be instrumented by Istanbul)
+            'app/!(bower_components)**/!(*_test).js': ['coverage']
+        },
 
-    plugins : [
+        // optionally, configure the reporter
+        coverageReporter: {
+            type : 'html',
+            dir : 'coverage/'
+        },
+
+        frameworks: ['jasmine'],
+
+        browsers: ['Chrome'],
+
+        plugins: [
             'karma-chrome-launcher',
             'karma-firefox-launcher',
             'karma-jasmine',
-            'karma-junit-reporter'
-            ],
+            'karma-junit-reporter',
+            'karma-coverage'
+        ],
 
-    junitReporter : {
-      outputFile: 'test_out/unit.xml',
-      suite: 'unit'
-    }
+        junitReporter: {
+            outputFile: 'test_out/unit.xml',
+            suite: 'unit'
+        }
 
-  });
+    });
 };
