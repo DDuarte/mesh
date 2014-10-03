1d3f233010f96a1e09ac089818b32f3a0a199f9f
RubenCordeiro <ruben.cordeiro.eng@gmail.com>
2014-10-03 08:06:11 +0100

    Replace the default router with angular-ui-router
    The angular-ui-router is more flexible and allows a better management of nested views. For more information: https://github.com/angular-ui/ui-router.

--- Client/app/app.js
+++ Client/app/app.js
@@ -2,11 +2,11 @@
 
 // Declare app level module which depends on views, and components
 angular.module('myApp', [
-  'ngRoute',
-  'myApp.view1',
-  'myApp.view2',
-  'myApp.version'
-]).
-config(['$routeProvider', function($routeProvider) {
-  $routeProvider.otherwise({redirectTo: '/view1'});
-}]);
+    'ui.router',
+    'myApp.view1',
+    'myApp.view2',
+    'myApp.version'
+])
+    .config(['$urlRouterProvider', function ($urlRouterProvider) {
+        $urlRouterProvider.otherwise("/view1");
+    }]);
--- Client/app/view1/view1.js
+++ Client/app/view1/view1.js
@@ -1,14 +1,15 @@
 'use strict';
 
-angular.module('myApp.view1', ['ngRoute'])
+angular.module('myApp.view1', ['ui.router'])
 
-.config(['$routeProvider', function($routeProvider) {
-  $routeProvider.when('/view1', {
-    templateUrl: 'view1/view1.html',
-    controller: 'View1Ctrl'
-  });
-}])
+    .config(['$stateProvider', function ($stateProvider) {
+        $stateProvider.state('view1', {
+            url: "/view1",
+            templateUrl: "view1/view1.html",
+            controller: "View1Ctrl"
+        });
+    }])
 
-.controller('View1Ctrl', [function() {
+    .controller('View1Ctrl', [function () {
 
-}]);
+    }]);
