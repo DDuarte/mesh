2487d20e1798f5c175fcca602985719050acfc21
DDuarte <dnpd.dd@gmail.com>
2014-10-09 16:43:38 +0100

    test commit

--- Client/src/app/model/model.js
+++ Client/src/app/model/model.js
@@ -12,7 +12,7 @@ angular.module('meshApp.model', [
                 camera.position.z = 1000;
 
                 geometry = new THREE.BoxGeometry(200, 200, 200);
-                material = new THREE.MeshBasicMaterial({ color: 0xff0000, wireframe: true });
+                material = new THREE.MeshBasicMaterial({ color: 0xff00ff, wireframe: true });
 
                 mesh = new THREE.Mesh(geometry, material);
                 scene.add(mesh);
