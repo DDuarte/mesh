690193cf272d5590bde9fa47e1c74798bce317f5
Luis <luisccleto@gmail.com>
2014-10-09 18:33:31 +0100

    Add mock login page

--- Client/src/app/login/login.js
+++ Client/src/app/login/login.js
@@ -12,37 +12,7 @@ angular.module('meshApp.login', [
     })
 
     .controller('LoginCtrl', function LoginController($scope) {
-        $scope.today = function() {
-            $scope.dt = new Date();
+        $scope.init = function () {
+            angular.element('body').css("background-color","#428bca");
         };
-        $scope.today();
-
-        $scope.clear = function () {
-            $scope.dt = null;
-        };
-
-        // Disable weekend selection
-        $scope.disabled = function(date, mode) {
-            return ( mode === 'day' && ( date.getDay() === 0 || date.getDay() === 6 ) );
-        };
-
-        $scope.toggleMin = function() {
-            $scope.minDate = $scope.minDate ? null : new Date();
-        };
-        $scope.toggleMin();
-
-        $scope.open = function($event) {
-            $event.preventDefault();
-            $event.stopPropagation();
-
-            $scope.opened = true;
-        };
-
-        $scope.dateOptions = {
-            formatYear: 'yy',
-            startingDay: 1
-        };
-
-        $scope.formats = ['dd-MMMM-yyyy', 'yyyy/MM/dd', 'dd.MM.yyyy', 'shortDate'];
-        $scope.format = $scope.formats[0];
     });
