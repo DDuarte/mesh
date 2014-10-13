b9e1e5282d807c547ed0c6d9690d1b516ab152f6
migulorama <migulorama@gmail.com>
2014-10-13 14:51:10 +0100

    Client: Remove karma execution from watch task.

--- Client/Gruntfile.js
+++ Client/Gruntfile.js
@@ -410,7 +410,7 @@ module.exports = function (grunt) {
                 files: [
                     '<%= app_files.js %>'
                 ],
-                tasks: [ 'jshint:src', 'karma:unit:run', 'copy:build_appjs' ]
+                tasks: [ 'jshint:src', 'copy:build_appjs' ]
             },
 
             /**
@@ -460,7 +460,7 @@ module.exports = function (grunt) {
                 files: [
                     '<%= app_files.jsunit %>'
                 ],
-                tasks: [ 'jshint:test', 'karma:unit:run' ],
+                tasks: [ 'jshint:test' ],
                 options: {
                     livereload: false
                 }
