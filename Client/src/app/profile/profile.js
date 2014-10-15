88ab99b0a1ef43815b15f6371f15723fbb7f44a6
DDuarte <dnpd.dd@gmail.com>
2014-10-15 02:42:43 +0100

    server: sample usage of Redis: profile page has a profile view counter for each user
    client: /#/profile/:username
    server: /users/:username

--- Client/src/app/profile/profile.js
+++ Client/src/app/profile/profile.js
@@ -3,12 +3,16 @@ angular.module('meshApp.profile', [
 ])
     .config(function config($stateProvider) {
         $stateProvider.state('home.profile', {
-            url: '/profile',
+            url: '/profile/:id',
             controller: 'ProfileCtrl',
             templateUrl: 'profile/profile.tpl.html',
             data: { pageTitle: 'profile' }
         });
     })
 
-    .controller('ProfileCtrl', function ProfileController($scope) {
+    .controller('ProfileCtrl', function ProfileController($scope, $stateParams, $http) {
+        $http.get('http://meshdev.ddns.net:8000/users/' + $stateParams.id). // TODO: make url configurable?
+            success(function (data, status, headers, config) {
+                $scope.user = data;
+            });
     });
--- Server/index.js
+++ Server/index.js
@@ -1,15 +1,40 @@
-var Hapi = require('hapi');
+var Hapi = require('hapi'),
+    redis = require('redis'),
+    client = redis.createClient();
+
+client.on('error', function (err) {
+    console.log('Redis error ' + err);
+});
 
 // Create a server with a host and port
-var server = new Hapi.Server('localhost', 8000);
+var server = new Hapi.Server(8000);
 
 // Add the route
 server.route({
     method: 'GET',
     path: '/',
     handler: function (request, reply) {
-        reply('hello world');
+        client.incr('counter', function (err, val) {
+            reply('Counter: ' + val);
+        });
     }
+});
+
+function getUser(request, reply) {
+    var id = request.params.id;
+
+    client.incr(id + '_counter', function (err, counter) {
+        reply({
+            'id': id,
+            'counter': counter
+        }).header('Access-Control-Allow-Origin', "*");
+    });
+}
+
+server.route({
+    method: 'GET',
+    path: '/users/{id}',
+    handler: getUser
 });
 
 // Start the server
