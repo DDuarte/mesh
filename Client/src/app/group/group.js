1c4a182701856028ebf6f6a0e23a3828d3798adb
migulorama <migulorama@gmail.com>
2014-10-15 12:27:19 +0100

    Client: Add initial design of the group page.

--- Client/src/app/group/group.js
+++ Client/src/app/group/group.js
@@ -13,4 +13,6 @@ angular.module('meshApp.group', [
 })
 
 .controller('GroupCtrl', function GroupController($scope) {
+        var models = function (g) { return _.range(10).map(function (i) { return { name: "Model " + g + "-" + i }; }); };
+        $scope.galleries = _.range(10).map(function (i) { return { name: "Gallery " + i, models: models(i) }; });
 });
