31e9b220cd03ba4ab6469bdd1b8af24bfd96347e
Luis <luisccleto@gmail.com>
2014-10-15 01:00:19 +0100

    Add placeholder model

--- Client/src/app/model/model.js
+++ Client/src/app/model/model.js
@@ -1,3 +1,29 @@
+var placeholderModel = {
+    "name": "The Box",
+    "files": "http://www.nowhereatall.com",
+    "upvotes": "20",
+    "downvotes": "2",
+    "authorName": "Donatello",
+    "authorAvatar": "http://i.imgur.com/KgKyXqN.jpg",
+    "authorAbout": "I like pizza and deep philosophical conundrums.",
+    "publicationDate": "2010-04-05T12:38:20.000Z",
+    "description" : "what have you done to me?\na miasma of darkness as perceptions crawl.\nonce we savored wonder,\ninnocent and untainted,\nbut your desire paled.\na feverish morass of darkness -\nmemories follow darkness, follow bitterness,\nlove dissolved.\nin a haze of sorrow,\nI see you.",
+    "comments" :[
+        {
+            "author": "Michelangelo",
+            "avatar": "http://i.imgur.com/PbgQGd1.png",
+            "date": "2011-10-05T14:45:00.000Z",
+            "content": "Slender beams of moonlight enter this darkened chamber as I kneel, always a slave, always lost, frozen here, waiting.\nHaloed forms wrought in panes of glass loom as dust dances in the air, forming an image in my mind, reaving my darkened soul.\nRealization dawning on my face.\nI raise my head, now submitting to this uncaring truth."
+        },
+        {
+            "author": "Leonardo",
+            "avatar": "http://i.imgur.com/EYi19tc.png?1",
+            "date": "2011-10-05T14:48:00.000Z",
+            "content": "Lol, wut?"
+        }
+    ]
+};
+
 angular.module('meshApp.model', [
     'ui.router'
 ])
