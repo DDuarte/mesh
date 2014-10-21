d4af5ed30c883deaa295ee76fdafd8afda7e4aa9
RubenCordeiro <ruben.cordeiro.eng@gmail.com>
2014-10-21 18:40:57 +0100

    Server: add getByName method to the model object

--- Server/models/model.js
+++ Server/models/model.js
@@ -34,5 +34,32 @@ model.getById = function (id) {
     });
 };
 
+/**
+ *
+ * Returns a model by it's name
+ * @param name
+ * @returns {Promise} Returns a promise with the resolved model, rejects to error otherwise
+ *
+ */
+model.getByName = function (name) {
+    return new Promise ( function (resolve, reject) {
+        var query = [
+            'MATCH (m:Model{name: {modelName}})<-[:OWNS]-(author)',
+            'MATCH m<-[c:COMMENTED]-(cAuthor)',
+            'WITH *',
+            'ORDER BY c.date DESC LIMIT 10',
+            'RETURN {name: m.name, description: m.description, files: m.files, downVotes: m.downvotes, upVotes: m.upvotes, publicationDate: m.publicationDate, visibility: m..visibility, tags: m.tags, author: { name: author.name, avatar: author.avatar, about: author.about }, comments: collect({ date: c.date, content: c.content, author: cAuthor.name, avatar: cAuthor.avatar }) } as model'
+        ].join('\n');
+
+        var params = {
+            modelName: name
+        };
+
+        db.query(query, params, function (err, results) {
+            if (err) return reject(err);
+            return resolve(results);
+        });
+    });
+};
 
 exports = model;
