9d07992f5a50b3a56e5bb26dd43c2589e2e39454
Luis <luisccleto@gmail.com>
2014-10-22 15:19:15 +0100

    Create model.getByProps in order to reuse query. Change model.getById and model.getByName to use this function.

--- Server/models/model.js
+++ Server/models/model.js
@@ -7,26 +7,28 @@ var db = new neo4j.GraphDatabase(
 );
 
 var model = {};
+
 /**
  *
- * Returns a model by it's identifier
- * @param {Number} id
+ * Returns a model by it's properties
+ * @param props
+ * @returns {Promise} Returns a promise with the resolved model, rejects to error otherwise
  *
  */
-model.getById = function (id) {
-    return new Promise( function (resolve, reject) {
+model.getByProps = function (props) {
+    return new Promise ( function (resolve, reject) {
         var query = [
-            'MATCH (m:Model{id : 1})<-[:OWNS]-(author)',
+            'MATCH (m:Model{properties})<-[:OWNS]-(author)',
             'MATCH m<-[c:COMMENTED]-(cAuthor)',
             'WITH *',
             'ORDER BY c.date DESC LIMIT 10',
             'WITH m, author, collect({ date: c.date, content: c.content, author: cAuthor.name, avatar: cAuthor.avatar }) AS modelComments',
             'MATCH m-[:TAGGED]->(modelTag:Tag)',
             'RETURN { name: m.name, description: m.description, files: m.files, downVotes: m.downvotes, upVotes: m.upvotes, publicationDate: m.publicationDate, visibility: m.visibility, tags: collect(modelTag.name), author: { name: author.name, avatar: author.avatar, about: author.about }, comments:  modelComments, tags: collect(modelTag.name)} AS model'
-            ].join('\n');
+        ].join('\n');
 
         var params = {
-            modelId: Number(id)
+            properties: props
         };
 
         db.query(query, params, function (err, results) {
@@ -38,30 +40,24 @@ model.getById = function (id) {
 
 /**
  *
+ * Returns a model by it's identifier
+ * @param {Number} modelId
+ *
+ */
+model.getById = function (modelId) {
+    return model.getByProps({id: modelId})
+};
+
+
+/**
+ *
  * Returns a model by it's name
- * @param name
+ * @param modelName
  * @returns {Promise} Returns a promise with the resolved model, rejects to error otherwise
  *
  */
-model.getByName = function (name) {
-    return new Promise ( function (resolve, reject) {
-        var query = [
-            'MATCH (m:Model{name: {modelName}})<-[:OWNS]-(author)',
-            'MATCH m<-[c:COMMENTED]-(cAuthor)',
-            'WITH *',
-            'ORDER BY c.date DESC LIMIT 10',
-            'RETURN {name: m.name, description: m.description, files: m.files, downVotes: m.downvotes, upVotes: m.upvotes, publicationDate: m.publicationDate, visibility: m..visibility, tags: m.tags, author: { name: author.name, avatar: author.avatar, about: author.about }, comments: collect({ date: c.date, content: c.content, author: cAuthor.name, avatar: cAuthor.avatar }) } as model'
-        ].join('\n');
-
-        var params = {
-            modelName: name
-        };
-
-        db.query(query, params, function (err, results) {
-            if (err) return reject(err);
-            return resolve(results);
-        });
-    });
+model.getByName = function (modelName) {
+    return model.getByProps({name: modelName})
 };
 
 module.exports = model;
