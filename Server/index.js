a56521f2b471f5e1ad02ca040c7ca5eca96de545
RubenCordeiro <ruben.cordeiro.eng@gmail.com>
2014-11-02 23:53:40 +0000

    Server: move the login logic to routes/login

--- Server/index.js
+++ Server/index.js
@@ -8,16 +8,17 @@ var _ = require('lodash'),
     redis = require('redis'),
     client = redis.createClient(),
     Model = require('./models/model'),
-    User = require('./models/user');
+    User = require('./models/user'),
+    Boom = require('boom');
 
 client.on('error', function (err) {
     console.log('Redis error ' + err);
 });
 
-
-
 // Create a server with a host and port
 var server = Hapi.createServer(process.argv[2] || 8001, { cors: true });
+
+require('./routes/')(server);
 
 // Test route
 server.route({
@@ -80,7 +81,7 @@ server.route({
         Model.getCommentsOlderThan(request.params.id, request.query.startdate).then(function (result) {
             reply(result);
         }, function (error) {
-            reply('Internal error').code(500);
+            reply(Boom.badImplementation('Internal Error'));
         });
     }
 });
@@ -124,81 +125,31 @@ server.route({
     }
 });
 
-var validate = function (rToken, decodedToken, callback) {
-    console.log(rToken);
-    console.log(decodedToken);
-
-    if (decodedToken) {
-        console.log(decodedToken.username.toString());
-    }
-
-    client.get(decodedToken.username, function (err, tok) {
-        if (err) return reply(err).code(500);
-        if (!tok) {
-            return callback(null, false);
-        } else if (tok == rToken) {
-            return callback(null, true, decodedToken);
-        } else {
-            return callback(null, false);
-        }
-    });
-};
-
-server.pack.register(require('hapi-auth-jsonwebtoken'), function (err) {
-
-    server.auth.strategy('token', 'jwt', {key: privateKey, validateFunc: validate});
-
-    server.route({
-        method: 'GET',
-        path: '/tokenRequired',
-        config: {auth: 'token'},
-        handler: function (request, reply) {
-            var replyObj = {
-                text: 'I am a JSON response, and you needed a token to get me.',
-                credentials: request.auth.credentials
-            };
-            reply(replyObj);
-        }
-    });
-
-    server.route({
-        method: 'GET',
-        path: '/noTokenRequired',
-        config: {auth: false},
-        handler: function (request, reply) {
-            var replyObj = {text: 'I am JSON response, but you did not need a token to get me'};
-            reply(replyObj);
-        }
-    });
-
-
-    server.route({
-        method: 'POST',
-        path: '/models/{id}/comments',
-        config: {
-            auth: 'token',
-            validate: {
-                params: {
-                    id: Joi.number().integer().min(1).required()
-                },
-                payload: {
-                    comment: Joi.string().min(1).max(1024).trim().required()
-                }
+server.route({
+    method: 'POST',
+    path: '/models/{id}/comments',
+    config: {
+        auth: 'token',
+        validate: {
+            params: {
+                id: Joi.number().integer().min(1).required()
+            },
+            payload: {
+                comment: Joi.string().min(1).max(1024).trim().required()
             }
-        },
-        handler: function (request, reply) {
-            Model.addComment(request.params.id, request.auth.credentials.username, request.payload.comment).then(function (result) {
-                if (result.length == 0) {
-                    reply('No such model.').code(404);
-                } else {
-                    reply(result);
-                }
-            }, function (error) {
-                reply('Internal error').code(500);
-            });
         }
-    })
-
+    },
+    handler: function (request, reply) {
+        Model.addComment(request.params.id, request.auth.credentials.username, request.payload.comment).then(function (result) {
+            if (result.length == 0) {
+                reply('No such model.').code(404);
+            } else {
+                reply(result);
+            }
+        }, function (error) {
+            reply('Internal error').code(500);
+        });
+    }
 });
 
 
@@ -211,7 +162,6 @@ server.pack.register({plugin: require('hapi-routes-status'), options: {path: '/s
     if (err)
         throw err;
 });
-
 
 // Start the server
 server.start();
--- Server/routes/login.js
+++ Server/routes/login.js
@@ -1,3 +1,5 @@
+'use strict';
+
 var User = require('../models/user');
 var Boom = require('boom');
 var Joi = require('joi');
@@ -8,6 +10,56 @@ var client = redis.createClient();
 var privateKey = 'Kitties';
 
 module.exports = function (server) {
+
+    var validate = function (rToken, decodedToken, callback) {
+        console.log(rToken);
+        console.log(decodedToken);
+
+        if (decodedToken) {
+            console.log(decodedToken.username.toString());
+        }
+
+        client.get(decodedToken.username, function (err, tok) {
+            if (err) return callback(null, false);
+            if (!tok) {
+                return callback(null, false);
+            } else if (tok == rToken) {
+                return callback(null, true, decodedToken);
+            } else {
+                return callback(null, false);
+            }
+        });
+    };
+
+    server.pack.register(require('hapi-auth-jsonwebtoken'), function (err) {
+
+        server.auth.strategy('token', 'jwt', {key: privateKey, validateFunc: validate});
+
+        server.route({
+            method: 'GET',
+            path: '/tokenRequired',
+            config: {auth: 'token'},
+            handler: function (request, reply) {
+                var replyObj = {
+                    text: 'I am a JSON response, and you needed a token to get me.',
+                    credentials: request.auth.credentials
+                };
+                reply(replyObj);
+            }
+        });
+
+        server.route({
+            method: 'GET',
+            path: '/noTokenRequired',
+            config: {auth: false},
+            handler: function (request, reply) {
+                var replyObj = { text: 'I am JSON response, but you did not need a token to get me' };
+                reply(replyObj);
+            }
+        });
+
+    });
+
     server.route({
         method: 'POST',
         path: '/login',
