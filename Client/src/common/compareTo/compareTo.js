0cdbddaac0dfe4c2b18c3de6ade3b0731968d555
Nayd <dnpd.dd@gmail.com>
2014-11-17 23:59:10 +0000

    Client: make sure confirm password is equal to password before submiting the form
    Store ID 82562598

---
+++ Client/src/common/compareTo/compareTo.js
@@ -0,0 +1,18 @@
+angular.module('meshApp').directive("compareTo", function() {
+    return {
+        require: "ngModel",
+        scope: {
+            otherModelValue: "=compareTo"
+        },
+        link: function(scope, element, attributes, ngModel) {
+
+            ngModel.$validators.compareTo = function(modelValue) {
+                return modelValue == scope.otherModelValue;
+            };
+
+            scope.$watch("otherModelValue", function() {
+                ngModel.$validate();
+            });
+        }
+    };
+});
