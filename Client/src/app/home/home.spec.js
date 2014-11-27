43091d3ace3b73a987c739d9a5111a920b06a069
Ruben_Cordeiro <ruben.cordeiro.eng@gmail.com>
2014-11-27 10:37:50 +0000

    Client: add test task, setup dummy test

--- Client/src/app/home/home.spec.js
+++ Client/src/app/home/home.spec.js
@@ -4,11 +4,12 @@
  * build process will exclude all `.spec.js` files from the build
  * automatically.
  */
-describe( 'home section', function() {
-  beforeEach( module( 'meshApp.home' ) );
+describe('home section', function () {
+    beforeEach(module('ui.router')); // this declaration is mandatory for the tests to work
+    beforeEach(module('meshApp.home'));
 
-  it( 'should have a dummy test', inject( function() {
-    expect( true ).toBeTruthy();
-  }));
+    it('should have a dummy test', inject(function () {
+        expect(true).toBeTruthy();
+    }));
 });
 
