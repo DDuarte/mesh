94b425d37ca2f98a091f4291a6c6dc2dbd14800f
Ruben_Cordeiro <ruben.cordeiro.eng@gmail.com>
2014-11-30 09:10:16 +0000

    Client: update the group create view

--- Client/src/app/groupCreate/groupCreate.js
+++ Client/src/app/groupCreate/groupCreate.js
@@ -3,21 +3,40 @@ angular.module('meshApp.groupCreate', [
 ])
     .config(function ($stateProvider) {
         $stateProvider.state('home.groupCreate', {
-            url: '/groupCreate/',
+            url: '/groupCreate',
             templateUrl: 'groupCreate/groupCreate.tpl.html',
             data: {
                 pageTitle: "Create Group"
             }
         });
     })
-    .controller("GroupCreateCtrl", function ($scope, $state, meshApi) {
-        $scope.createGroup = function (groupName) {
-            meshApi.createGroup(groupName)
-                .success(function () {
+    .controller("GroupCreateCtrl", function ($scope, $state, meshApi, usSpinnerService, ngDialog) {
+        $scope.group = {};
+        $scope.registerPending = false;
+        $scope.createGroup = function (group) {
+            usSpinnerService.spin('spinner');
+            $scope.registerPending = true;
+
+            meshApi.createGroup(group.name, group.description)
+                .success(function (data) {
+                    usSpinnerService.stop('spinner');
+                    $scope.registerPending = false;
+
+                    ngDialog.openConfirm({
+                        template: 'groupCreateSuccessId',
+                        className: 'ngdialog-theme-default',
+                        showClose: false
+                    }).then(function() {
+                        $state.go('home.group', {
+                            id: data.id
+                        });
+                    });
 
                 })
-                .error(function (data, status, headers, config) {
-
+                .error(function (data) {
+                    usSpinnerService.stop('spinner');
+                    $scope.registerPending = false;
+                    $scope.groupRegisterError = data.error;
                 });
         };
     });
