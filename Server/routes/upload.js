5e629ce2eb90348371969d6f506119564740e03b
Ruben_Cordeiro <ruben.cordeiro.eng@gmail.com>
2014-11-30 22:47:46 +0000

    Server: update /upload route

--- Server/routes/upload.js
+++ Server/routes/upload.js
@@ -26,31 +26,40 @@ module.exports = function (server) {
             var data = request.payload;
             var ownerName = request.auth.credentials.username;
 
-            if (data.file) {
-                var name = data.file.hapi.filename;
-                var path = __dirname + "/models/" + name;
-                var file = Fs.createWriteStream(path);
+            Model.getByName(request.payload.name)
+                .then(function () {
+                    reply(Boom.badRequest('A model with that name already exists'));
+                })
+                .catch(Error, function (error) {
+                    reply(Boom.badImplementation(error.message));
+                })
+                .catch(function () {
+                    if (data.file) {
+                        var name = data.file.hapi.filename;
+                        var path = __dirname + "/models/" + name;
+                        var file = Fs.createWriteStream(path);
 
-                file.on('error', function (err) {
-                    console.error(err)
-                });
-
-                data.file.pipe(file);
-
-                data.file.on('end', function (err) {
-
-                    if (err)
-                        return reply(Boom.badImplementation("Error saving file on the server"));
-
-                    Model.create(request.payload.name, request.payload.description, path, ownerName)
-                        .then(function (model) {
-                            reply(model).code(200);
-                        })
-                        .catch(Error, function (error) {
-                            reply(Boom.badImplementation(error.message));
+                        file.on('error', function (err) {
+                            console.error(err)
                         });
+
+                        data.file.pipe(file);
+
+                        data.file.on('end', function (err) {
+
+                            if (err)
+                                return reply(Boom.badImplementation("Error saving file on the server"));
+
+                            Model.create(request.payload.name, request.payload.description, path, ownerName)
+                                .then(function (model) {
+                                    reply(model).code(200);
+                                })
+                                .catch(Error, function (error) {
+                                    reply(Boom.badImplementation(error.message));
+                                });
+                        });
+                    }
                 });
-            }
         }
     });
 };
