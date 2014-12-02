80efbb49766e46fe9b259ae7b9816cb0689b9520
RubenCordeiro <ruben.cordeiro.eng@gmail.com>
2014-12-02 21:39:01 +0000

    Server: add missing model schema fields

--- Server/schema/model.js
+++ Server/schema/model.js
@@ -2,6 +2,8 @@ var Joi = require('joi');
 
 module.exports = {
     id: Joi.number().integer().min(1),
+    name: Joi.string().min(5).max(20),
+    description: Joi.string().min(10).max(254),
     date: Joi.string().isoDate(),
     comment: Joi.string().trim().min(1).max(1024),
     vote: Joi.string().trim().regex(/(DOWN)|(UP)/)
