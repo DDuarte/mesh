f450a1b1a736bc14537a3c2b17804b91666394b8
Ruben_Cordeiro <ruben.cordeiro.eng@gmail.com>
2014-12-28 15:34:19 +0000

    Server: update search route

--- Server/routes/search.js
+++ Server/routes/search.js
@@ -4,6 +4,7 @@ var Promise = require('bluebird');
 var Group = require('../models/group');
 var Model = require('../models/model');
 var User = require('../models/user');
+var Boom = require('boom');
 
 module.exports = function (server) {
     server.route({
@@ -14,13 +15,19 @@ module.exports = function (server) {
         },
         handler: function (request, reply) {
             var term = request.params.term;
-            Promise.join(Model.searchByName(term), Group.getByName(term), User.getByUsername(term),
+            Promise.join(Model.searchByName(term), Group.searchByName(term), User.searchByUsername(term),
                 function (models, groups, users) {
+                    console.log("here");
+                    console.log("models", models);
                     return reply({
                         models: models,
                         groups: groups,
                         users: users
                     });
+                })
+                .catch(function(error) {
+                    console.log("Error:", error);
+                    return reply(Boom.badImplementation('Internal server error'));
                 });
         }
     })
