214136b757773ae02432feccc9b6c667964eb1ef
Pedro Fernandes <pedrorfernandes@yahoo.com>
2014-12-29 15:56:46 +0000

    server: created notification model function and added a simple POST method to test notification insertion, /notification/test

--- Server/models/notification.js
+++ Server/models/notification.js
@@ -1,7 +1,10 @@
 var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
+var Promise = require('bluebird');
 
-var notificationSchema = new mongoose.Schema({
+var exports = {};
+
+var notificationsSchema = new mongoose.Schema({
     userFrom: String,
     userTo: String,
     url: String,
@@ -11,4 +14,42 @@ var notificationSchema = new mongoose.Schema({
     date: Date
 });
 
-module.exports = mongoose.model('Notification', notificationSchema);
+var Notification = mongoose.model('Notification', notificationsSchema);
+
+exports.notifyFollowers = function(user, message, url, image) {
+    
+};
+
+exports.notifyUser = function(userTo, userFrom, message, url, image) {
+    var notification = new Notification({
+        userFrom: userFrom.username,
+        userTo: userTo.username,
+        url: url,
+        image: image,
+        message: message,
+        seen: false,
+        date: new Date()
+    });
+
+    return new Promise(function (resolve, reject) {
+        notification.save(function (error, savedNotification) {
+            if (error) return reject(error);
+            return resolve(savedNotification);
+        });
+    });
+};
+
+exports.notifyGroup = function(group, userFrom, message, url, image) {
+    
+};
+
+exports.remove = function(exports) {
+    
+};
+
+exports.update = function(updatedNotification) {
+    
+};
+
+// module.exports = mongoose.model('Notification', notificationsSchema);
+module.exports = exports;
