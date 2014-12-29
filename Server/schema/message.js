098cf23091e3ba2c6ef20f203994f4225c1d9e02
Ruben_Cordeiro <ruben.cordeiro.eng@gmail.com>
2014-12-29 09:56:06 +0000

    Server: update message schema

--- Server/schema/message.js
+++ Server/schema/message.js
@@ -3,6 +3,6 @@ var Joi = require('joi');
 module.exports = {
     userTo: Joi.string(),
     userFrom: Joi.string(),
-    content: Joi.string().min(10).max(254),
-    title: Joi.string().min(5).max(20),
+    content: Joi.string().min(10).max(1024),
+    title: Joi.string().min(5).max(20)
 };
