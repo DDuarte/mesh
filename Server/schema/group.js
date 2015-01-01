a46d655c90a0b05407657b866520cb0130a687f4
Luís Cleto <luisccleto@gmail.com>
2015-01-01 18:28:21 +0000

    Server: add visibility to group schema

--- Server/schema/group.js
+++ Server/schema/group.js
@@ -4,6 +4,7 @@ module.exports = {
     id: Joi.number().integer().min(1),
     name: Joi.string().min(5).max(20),
     description: Joi.string().min(10).max(254),
+    visibility: Joi.string().regex(/(public)|(private)/),
     creationDate: Joi.string().isoDate(),
     galleryName: Joi.string().min(5).max(20)
 };
