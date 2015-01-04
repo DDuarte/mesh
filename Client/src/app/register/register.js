b3ff4758fe4c8bf633a50f2b7add2c45a597960a
Nayd <dnpd.dd@gmail.com>
2015-01-04 22:44:46 +0000

    Revert "Client: Replace datepicker by input type="date""
    This reverts commit 7621e6397ad447e0df8040548dcb43330d74e6f4.

--- Client/src/app/register/register.js
+++ Client/src/app/register/register.js
@@ -48,6 +48,76 @@ angular.module('meshApp.register', [
 
             meshApi.register($scope.registerInfo).success(success).error(error);
         };
+
+        $scope.isOpen = false;
+    })
+
+    .directive('pickADate', function () {
+        return {
+            restrict: "A",
+            scope: {
+                pickADate: '=',
+                minDate: '=',
+                maxDate: '=',
+                pickADateOptions: '='
+            },
+            link: function (scope, element/*, attrs*/) {
+                var options = $.extend(scope.pickADateOptions || {}, {
+                    onSet: function (e) {
+                        if (scope.$$phase || scope.$root.$$phase) {// we are coming from $watch or link setup
+                            return;
+                        }
+                        var select = element.pickadate('picker').get('select'); // selected date
+                        scope.$apply(function () {
+                            if (e.hasOwnProperty('clear')) {
+                                scope.pickADate = null;
+                                return;
+                            }
+                            if (!scope.pickADate) {
+                                scope.pickADate = new Date(0);
+                            }
+                            scope.pickADate.setYear(select.obj.getFullYear());
+                            // Interesting: getYear returns only since 1900. Use getFullYear instead.
+                            // It took me half a day to figure that our. Ironically setYear()
+                            // (not setFullYear, duh) accepts the actual year A.D.
+                            // So as I got the $#%^ 114 and set it, guess what, I was transported to ancient Rome 114 A.D.
+                            // That's it I'm done being a programmer, I'd rather go serve Emperor Trajan as a sex slave.
+                            scope.pickADate.setMonth(select.obj.getMonth());
+                            scope.pickADate.setDate(select.obj.getDate());
+                        });
+                    },
+                    onClose: function () {
+                        element.blur();
+                    }
+                });
+                element.pickadate(options);
+                function updateValue(newValue) {
+                    if (newValue) {
+                        scope.pickADate = (newValue instanceof Date) ? newValue : new Date(newValue);
+                        // needs to be in milliseconds
+                        element.pickadate('picker').set('select', scope.pickADate.getTime());
+                    } else {
+                        element.pickadate('picker').clear();
+                        scope.pickADate = null;
+                    }
+                }
+                updateValue(scope.pickADate);
+                element.pickadate('picker').set('min', scope.minDate ? scope.minDate : false);
+                element.pickadate('picker').set('max', scope.maxDate ? scope.maxDate : false);
+                scope.$watch('pickADate', function (newValue, oldValue) {
+                    if (newValue == oldValue) {
+                        return;
+                    }
+                    updateValue(newValue);
+                }, true);
+                scope.$watch('minDate', function (newValue, oldValue) {
+                    element.pickadate('picker').set('min', newValue ? newValue : false);
+                }, true);
+                scope.$watch('maxDate', function (newValue, oldValue) {
+                    element.pickadate('picker').set('max', newValue ? newValue : false);
+                }, true);
+            }
+        };
     })
 
     .directive('countrySelect', ['$parse', function ($parse) {
@@ -106,3 +176,4 @@ angular.module('meshApp.register', [
             }
         };
     }]);
+
