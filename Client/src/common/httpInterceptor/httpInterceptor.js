d7c2be3cc3b9f2c3bdc7381e80abc00541a5c397
Ruben_Cordeiro <ruben.cordeiro.eng@gmail.com>
2015-01-06 00:07:58 +0000

    Client: remove unnecessary console.log

--- Client/src/common/httpInterceptor/httpInterceptor.js
+++ Client/src/common/httpInterceptor/httpInterceptor.js
@@ -1,7 +1,6 @@
 angular.module('meshApp').factory('httpInterceptor', function httpInterceptor ($q, $window, $location) {
     return {
         'responseError': function(rejection) {
-            console.log("foda-se", rejection);
 
             if (rejection.status === 401) {
                 $location.url('/login');
